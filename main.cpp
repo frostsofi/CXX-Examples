f29619ea6df9931a28f295249944f4fb373e42df
frostsofi <33616414+frostsofi@users.noreply.github.com>
2025-10-13 23:51:16 +0700

    Rvalue and lvalue reference example

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+void TestValueType(const std::string& iString) {
+    std::cout << "Lvalue reference" << std::endl;
+}
+
+void TestValueType(const std::string&& iString) {
+    std::cout << "Rvalue reference" << std::endl;
+}
+
 int main() 
 {
-    std::cout << "Hello!" << std::endl;
+    TestValueType("Hello!");
+
+    std::string test = "Hello!";
+    TestValueType(test);
+    TestValueType(std::move(test));
+
+    return 0;
 }
