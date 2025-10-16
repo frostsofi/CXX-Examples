e26fdecb9f4cfdb9421e044490dc8f5265c8ac4c
frostsofi <33616414+frostsofi@users.noreply.github.com>
2025-10-17 00:02:01 +0700

    Added examples with enum and enum class Added example with underlying type of enum

--- main.cpp
+++ main.cpp
@@ -8,13 +8,52 @@ void TestValueType(const std::string&& iString) {
     std::cout << "Rvalue reference" << std::endl;
 }
 
+enum Color {
+    Orange,
+    Yellow
+};
+
+ enum Fruit {
+   //Orange,
+   Apple
+};
+
+enum class ColorType {
+    Orange,
+    Yellow
+};
+
+enum class FruitType {
+   Orange,
+   Apple
+};
+
+enum class DeviceType : char {
+   Phone = 10,
+   // Laptop = 300
+};
+
 int main() 
 {
-    TestValueType("Hello!");
 
+    // Expression types
+    TestValueType("Hello!");
     std::string test = "Hello!";
     TestValueType(test);
     TestValueType(std::move(test));
+
+    // Enum 
+    Color curColor = Color::Orange;
+    if (curColor == Fruit::Apple) {
+        std::cout << "Enum problem with implicit comparison !" << std::endl;
+    }
+
+    ColorType color = ColorType::Orange;
+    FruitType fruit = FruitType::Orange;
+
+    //if (color == fruit) {
+    //    std::cout << "Enum problem with implicit comparison !" << std::endl;
+    //}
 
     return 0;
 }
