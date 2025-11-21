0630d39cd0e61822749c2db295004e0758d3d5c8
frostsofi <33616414+frostsofi@users.noreply.github.com>
2025-11-22 00:38:44 +0700

    Extended constexpr tests

--- memoryModel/constexprTest.h
+++ memoryModel/constexprTest.h
@@ -1,8 +1,16 @@
 #include <string>
 #include <iostream>
+#include <random>
 
 constexpr int GetArrSize(int sizeOfRow) {
     return sizeOfRow * 2;
+}
+
+int GetArrSizeRandom() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(1, 6);
+    return distrib(gen);
 }
 
 struct Client {
@@ -10,9 +18,39 @@ struct Client {
     int cardNumber;
 };
 
+class Shop {
+public:
+    constexpr Shop() : id(5), typeOfProducts{1,2,2,2,3}
+    {}
+
+    constexpr int GetId() const {
+        return id;
+    }
+
+    // since c++20 we can use constrexpr std::string version
+    // and have virtual constexpr method
+    constexpr virtual std::string GetName() const {
+        return "Cacao";
+    }
+
+   constexpr ~Shop() {}
+
+private:
+    int id;
+    int typeOfProducts[5];
+    // std::stack<int> values; non literal type 
+};
+
 void ConstexprTest() 
 {
     constexpr int matrix[GetArrSize(2)] = {1,0,0,1};
+
+    // test constexpr function with non constexpr params
+    int randomArrSize = GetArrSize(GetArrSizeRandom());
+
+    //constexpr int matrix[GetArrSizeRandom()];
     constexpr Client cl{2, 330};
+    constexpr Shop shop;
+    constexpr int shopId = shop.GetId();
     // constexpr auto [id, cardNumber] = cl; only with CXX 26 compiler support
 }
